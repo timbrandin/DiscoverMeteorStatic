1139bae8aa4ac596461e4bd0725bc85d42e1261d
Sacha Greif <sacha357@gmail.com>
2014-04-03 18:47:48 +0900

    changing language env var

--- change_lang.sh
+++ change_lang.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-array=(de en es fr gr id it jp kr nl pl pt ru zh ro da)
+array=(de en es fr gr id it jp kr nl pl pt ru zh ro ar da)
 for i in "${array[@]}"
 do
   echo "Changing LANG to LANGUAGE for language $i"
-  heroku config:set LANGUAGE=$i --app discover-meteor-$i
+  heroku config:set DMLANG=$i --app discover-meteor-$i
 done
