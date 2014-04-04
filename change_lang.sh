3a35d5d3c8d1d2fa471b8da2fbd0fe59d8004fc2
Sacha Greif <sacha357@gmail.com>
2014-04-04 09:44:48 +0900

    added arabic strings

--- change_lang.sh
+++ change_lang.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-array=(de en es fr gr id it jp kr nl pl pt ru zh ro ar da)
+array=(de en es fr gr id it jp kr nl pl pt ru zh ro ar da hr ms tr am uk th)
 for i in "${array[@]}"
 do
-  echo "Changing LANG to LANGUAGE for language $i"
+  echo "Setting DMLANG for language $i"
   heroku config:set DMLANG=$i --app discover-meteor-$i
 done
